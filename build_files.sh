8c9b933a40febb3ca2912ed997fafc3d36339b3c
Harshita Jain <harshita.jain@gagahealth.com>
2024-06-16 16:12:10 +0530

    new virtual env

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 echo "------------------------------BUILD STARTED-------------------------------"
 
+
+python3 -m pip install virtualenv
 source myenv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
