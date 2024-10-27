d4db7ca29a71f968cd813e7e3590a8d6accdb37c
Harshita Jain <harshita.jain@gagahealth.com>
2024-10-27 20:55:40 +0530

    setting env variables

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -148,8 +148,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Example: 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'harshitajn18@gmail.com'
-EMAIL_HOST_PASSWORD = 'aind wiuf gqwp gwxi'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
