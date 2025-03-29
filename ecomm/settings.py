dfaa4911db596a2216fd05f534be0e1d827e39d2
Harshita Jain <harshita.jain@gagahealth.com>
2025-03-29 13:51:56 +0530

    changes in docker compose for env variables

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,11 +150,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Example: 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-RAZORPAY_ID = os.environ.get('RAZORPAY_ID')
-RAZORPAY_SECRET = os.environ.get('RAZORPAY_SECRET')
+RAZORPAY_ID = os.getenv('RAZORPAY_ID')
+RAZORPAY_SECRET = os.getenv('RAZORPAY_SECRET')
 
 
 # Static files (CSS, JavaScript, Images)
