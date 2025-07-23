d80af899e75a1137f1958287a7d33b69cc43cba5
Harshita Jain <harshita.jain@gagahealth.com>
2025-07-23 17:59:23 +0530

    more changes

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -173,6 +173,5 @@ MEDIA_URL = '/media/'
 
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static', 'staticfiles_build')
